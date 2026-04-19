a76d3e2eb97fb7b2ba73b28d6092164721c3732c
Kyaw Min Htwe <kyawminhtway288@gmail.com>
2026-04-19 12:59:47 +0630

    Fix: replace next.config.ts with next.config.js

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true, // Set to true if you want to avoid Vercel image optimization costs
-  },
-};
-
-export default nextConfig;
