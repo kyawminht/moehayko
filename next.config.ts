e6b858cfd7da124c39536e2049cd2424aa188f75
Kyaw Min Htwe <kyawminhtway288@gmail.com>
2026-04-19 12:54:50 +0630

    fix:add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // Set to true if you want to avoid Vercel image optimization costs
+  },
 };
 
 export default nextConfig;
